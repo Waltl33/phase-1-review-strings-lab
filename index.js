2f6c6f4d2daca9ca29dc7c9d2228ec11b15c7e2f
Walter <walter.clark24@yahoo.com>
2023-04-11 10:54:16 -0400

    commit tree 32c0a5e16210c7a8e455193782b5b69d4f531068

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+//let fruits = []
+//fruits = ["banana", "apple", "orange"]
+
+//let person = {name: "Brooke", age: 40, hobby: "running stupidly long distances"}
+
+//console.log(person)
+//console.log(person.name)
+const currentUser = "Grace Hopper"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice (0,1)}!`
